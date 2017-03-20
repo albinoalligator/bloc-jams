a6b89f1df993d484399eb2deb79b4e854d6f08fc
Elizabeth Erdman <elizabethjerdman@gmail.com>
2017-03-19 20:06:41 -0400

    fixes made to checkpoint 11, now complete

--- scripts/album.js
+++ scripts/album.js
@@ -1,68 +1,69 @@
-var albumPicasso = {
-    title: 'The Colors',
-    artist: 'Pablo Picasso',
-    label: 'Cubism',
-    year: '1881',
-    albumArtUrl: 'assets/images/album_covers/01.png',
-    songs: [
-        { title: 'Blue', duration: '4:26'},
-        { title: 'Green', duration: '3:14'},
-        { title: 'Red', duration: '5:01'},
-        { title: 'Pink', duration: '3:21'},
-        { title: 'Magenta'. duration: '2:15'}
-    ]
-};
 
-var albumMarconi = {
-    title: 'The Telephone',
-    artist: 'Guglielmo Marconi',
-    label: 'EM',
-    year: '1909',
-    albumArtUrl: 'assets/images/album_covers/20.png',
-    songs: [
-        { title: 'Hello, Operator?', duration: '1:01'},
-        { title: 'Ring, ring, ring', duration: '5:01'},
-        { title: 'Fits in your pocket', duration: '3:21'},
-        { title: 'Can you hear me now?', duration: '3:14'},
-        { title: 'Wrong phone number', duration: '2:15'}
-    ]
-};
+ var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+ 
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
 
-var createSongRow = function(songNumber, songName, songLength){
-    var template =
+var createSongRow = function(songNumber, songName, songLength) {
+     var template =
         '<tr class="album-view-song-item">'
-    +   '   <td class="song-item-number">' + songNumber + '</td>'
-    +   '   <td class="song-item-title">'  + songName   + '</td>'
-    +   '   <td class="song-item-duration">' + songLength + '</td>'
-    +   '</tr>'
-    ;
-    
-    return template;
-};
+      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-title">' + songName + '</td>'
+      + '  <td class="song-item-duration">' + songLength + '</td>'
+      + '</tr>'
+      ;
+ 
+     return template;
+ };
 
-var setCurrentAlbum = function(album){
-    //assign current values of album object properties to the HTML elements
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-    
-    //use firstChild to identify the first child node of an element and then nodeValue sets the value of that element
-    albumTitle.firstChild.nodeValue = album.title;
-    albumArtist.firstChild.nodeValue = album.artist;
-    albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-    albumImage.setAttribute('src', album.albumArtUrl);
-    
-    //clear the album song list's HTML so nothing is interferring 
-    albumSongList.innerHTML = '';
-    
-    //this loop goes thru all the songs from the album paramenter and inserts them into the HTML with the innerHTML property, at each loop, createSongRow passes in the song number, name and duration from the album object
-    for(var i = 0; i < album.songs.length; i++){
-        albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
-    }
-};
-
-window.onload = function() {
-    setCurrentAlbum(albumPicasso);
-};
+ var setCurrentAlbum = function(album) {
+     // #1
+     var albumTitle = document.getElementsByClassName('album-view-title')[0];
+     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+     var albumImage = document.getElementsByClassName('album-cover-art')[0];
+     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ 
+     // #2
+     albumTitle.firstChild.nodeValue = album.title;
+     albumArtist.firstChild.nodeValue = album.artist;
+     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+     albumImage.setAttribute('src', album.albumArtUrl);
+ 
+     // #3
+     albumSongList.innerHTML = '';
+ 
+     // #4
+     for (var i = 0; i < album.songs.length; i++) {
+         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+     }
+ };
+ 
+ window.onload = function() {
+     setCurrentAlbum(albumPicasso);
+ };
