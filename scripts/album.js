a4a0e05bca74217859e769dd46cbc2c409be4b9f
Elizabeth Erdman <elizabethjerdman@gmail.com>
2017-03-26 17:27:17 -0400

    changes made to checkpoint 12

--- scripts/album.js
+++ scripts/album.js
@@ -126,8 +126,7 @@ var currentPlayingSong = null;
      songListContainer.addEventListener('mouseover', function(event){
          
         if(event.target.parentElement.className === 'album-view-song-item'){
-              event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
-            
+                  
         var songItem = getSongItem(event.target);
             
         if (songItem.getAttribute('data-song-number') !== currentPlayingSong){
