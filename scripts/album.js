57b5fcef990c7cebcedcff068a47676d0275b056
Elizabeth Erdman <elizabethjerdman@gmail.com>
2017-03-28 19:52:05 -0400

    assignment 13 attempted

--- scripts/album.js
+++ scripts/album.js
@@ -64,8 +64,11 @@ var createSongRow = function(songNumber, songName, songLength) {
  };
 
 var findParentByClassName = function(element, targetClass) {
-    if (element) {
+    if (element.parentElement === undefined) {
+        console.log("No parent found");
+    } else if (element.parentElement !== undefined){
         var currentParent = element.parentElement;
+        
         while (currentParent.className !== targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
         }
