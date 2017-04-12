250c80828237474387a01de8496ffe48c6709f93
Elizabeth Erdman <elizabethjerdman@gmail.com>
2017-04-12 09:11:13 -0400

    checkpoint 16 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,33 +1,37 @@
  //Javascript
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points){
-          
-  var revealPoint= function(index) {
-      points[index].style.opacity=1;
-      points[index].style.transform="scaleX(1) translateY(0)";
-      points[index].style.msTransform="scaleX(1) translateY(0)";
-      points[index].style.WebkitTransform="scaleX(1) translateY(0)";
+var animatePoints = function(){
+    var revealPoint = function(){
+        //makes styling the property simple
+        //since this is wrapped in jQ method it references a different point element each time and executes the revealPoint callback
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
                 
-    }        
-            
-for(var i = 0; i < points.length; i++){
-     revealPoint(i);
-    }
+    }; 
+    
+    //each method iterates over each point and executes a callback
+    $.each($('.point'), revealPoint);
 };
 
-window.onload = function () {
-    if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
-     }
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+//window becomes a jQ object, load method takes function as an argument
+$(window).load(function(){
     
-    window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
+//jQ height property gets the objects height (window)
+   if($(window).height() > 950) {
+       animatePoints();  
+     }
+//uses jQ offset method    
+   var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    
+//jQ scroll method is an event listener but it wraps the appearance of events    
+    $(window).scroll(function(event){
+        
+       if($(window).scrollTop() >= scrollDistance){
+           animatePoints();
+          
          }
         
     });
-}
+});
 
