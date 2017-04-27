32969169082578875227ac3d937142d38dcbc8ad
Elizabeth Erdman <elizabethjerdman@gmail.com>
2017-04-27 08:32:33 -0400

    checkpoint 18 completed

--- scripts/album.js
+++ scripts/album.js
@@ -38,7 +38,60 @@ var createSongRow = function(songNumber, songName, songLength) {
       + '</tr>'
       ;
  
-     return $(template);
+     var $row = $(template);
+    
+    
+    var clickHandler = function(){
+        var songNumber = $(this).attr('data-song-number');
+        
+        if(currentPlayingSong !== null) {
+            
+            var currentPlayingCell = $('.song-item-number[data-song-number=" ' + currentPlayingSong + '"]');
+            currentPlayingCell.html(currentPlayingSong);        
+                
+        }
+        
+        if(currentPlayingSong !== songNumber) {
+            $(this).html(pauseButtonTemplate);
+            currentPlayingSong = songNumber;
+            
+        }else if (currentPlayingSong === songNumber){
+            $(this).html(playButtonTemplate);
+            currentPlayingSong = null;
+        }
+        
+        
+        
+    };
+    
+    
+      var onHover= function(event){
+          var songNumberCell = $(this).find('.song-item-number');
+          var songNumber = songNumberCell.attr('data-song-number');
+          
+          if( songNumber !== currentPlayingSong){
+              songNumberCell.html(playButtonTemplate);
+              
+          }   
+          
+      };
+      
+      var offHover = function(event){
+          var songNumberCell = $(this).find('.song-item-number');
+          var songNumber = songNumberCell.attr('data-song-number');
+          
+          if(songNumber !== currentPlayingSong){
+              songNumberCell.html(songNumber);
+          }
+          
+      }
+    
+    //find selector finds the element with the song-item-number class thats contained in whatever row is clicked, the click listen executes a call back function when the target ele is clicked
+    $row.find('.song-item-number').click(clickHandler);
+    //callbacks that execute when the mouse is over the $row and when it leaves
+    $row.hover(onHover, offHover);
+    
+    return $row;
  };
 
  var setCurrentAlbum = function(album) {
@@ -63,56 +116,8 @@ var createSongRow = function(songNumber, songName, songLength) {
      }
  };
 
-var findParentByClassName = function(element, targetClass) {
-    if (element) {
-        var currentParent = element.parentElement;
-        while (currentParent.className !== targetClass && currentParent.className !== null) {
-            currentParent = currentParent.parentElement;
-        }
-        return currentParent;
-    }
-};
-
-//uses the elements class names and returns the element with the .song-item-number class
-var getSongItem = function(element) {
-    switch (element.className) {
-        case 'album-song-button':
-        case 'ion-play':
-        case 'ion-pause':
-            return findParentByClassName(element, 'song-item-number');
-        case 'album-view-song-item':
-            return element.querySelector('.song-item-number');
-        case 'song-item-title':
-        case 'song-item-duration':
-            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
-        case 'song-item-number':
-            return element;
-        default:
-            return;
-    }  
-};
-
-var clickHandler = function(targetElement){
-    var songItem = getSongItem(targetElement);
-   //why is it initially set to null? 
-    if(currentPlayingSong === null){
-        songItem.innerHTML = pauseButtonTemplate;
-        currentPlayingSong = songItem.getAttribute('data-song-number');
-    } else if (currentPlayingSong === songItem.getAttribute('data-song-number')){
-        songItem.innerHTML = playButtonTemplate;
-        currentPlayingSong = null;
-    } else if (currentPlayingSong !== songItem.getAttribute('data-song-number')){
-        var currentPlayingSongElement = document.querySelector('[data-song-number="' + currentPlayingSong + '"]');
-        currentPlayingSongElement.innerHTML = currentPlayingSongElement.getAttribute('data-song-number');
-        songItem.innerHTML = pauseButtonTemplate;
-        currentPlayingSong = songItem.getAttribute('data-song-number');
-    }
-};
 
 
-var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
-
-var songRows = document.getElementsByClassName('album-view-song-item');
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
@@ -120,48 +125,10 @@ var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause">
 //store state of playing song
 var currentPlayingSong = null;
  
- window.onload = function() {
+ $(document).ready(function() {
      setCurrentAlbum(albumPicasso);
      
-     songListContainer.addEventListener('mouseover', function(event){
-         
-        if(event.target.parentElement.className === 'album-view-song-item'){
-                  
-        var songItem = getSongItem(event.target);
-            
-        if (songItem.getAttribute('data-song-number') !== currentPlayingSong){
-            songItem.innerHTML = playButtonTemplate;
-            }
-            
-        }
-         
-     });
-     
-      for (var i = 0; i < songRows.length; i++) {
-         songRows[i].addEventListener('mouseleave', function(event) {
-             
-                //cached song item
-                var songItem = getSongItem(event.target);
-                var songItemNumber = songItem.getAttribute('data-song-number');
-                
-                //checks to see that the iteam the mouse is leaving is not the current song and only changes content if it isnt
-                if (songItemNumber !== currentPlayingSong){
-                    songItem.innerHTML = songItemNumber;
-                }
-                                           
-         });
-          
-          //will eventually change the value of currently playing song
-          songRows[i].addEventListener('click', function(event){
-          
-          clickHandler(event.target);
-          
-      });  
-          
-        
-     }                                  
-     
- };
+ });
 
 
 
